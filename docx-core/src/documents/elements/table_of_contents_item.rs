3a7b925554813fe1e2f9a0a5e30531460979229a
Nikita Volobuev <nikitavbv@gmail.com>
2024-01-07 23:29:22 +0200

    Align left table of contents

--- docx-core/src/documents/elements/table_of_contents_item.rs
+++ docx-core/src/documents/elements/table_of_contents_item.rs
@@ -63,7 +63,7 @@ impl BuildXML for Vec<TableOfContentsItem> {
             .open_structured_tag_content();
 
         for (i, t) in self.iter().enumerate() {
-            let mut p = Paragraph::new().style(&format!("ToC{}", t.level));
+            let mut p = Paragraph::new().style(&format!("ToC{}", t.level)).align(AlignmentType::Left);
             if i == 0 {
                 p = p.unshift_run(
                     Run::new()
@@ -104,7 +104,7 @@ impl BuildXML for Vec<TableOfContentsItem> {
                 }
                 b = b.add_child(&p);
             } else {
-                let mut p = Paragraph::new().style(&format!("ToC{}", t.level));
+                let mut p = Paragraph::new().style(&format!("ToC{}", t.level)).align(AlignmentType::Left);
                 p = p.add_tab(
                     Tab::new()
                         .val(TabValueType::Right)
@@ -140,7 +140,7 @@ impl BuildXML for Vec<TableOfContentsItem> {
             }
 
             if i == self.len() - 1 {
-                let mut p = Paragraph::new().style(&format!("ToC{}", t.level));
+                let mut p = Paragraph::new().style(&format!("ToC{}", t.level)).align(AlignmentType::Left);
                 p = p.add_run(Run::new().add_field_char(FieldCharType::End, false));
                 b = b.add_child(&p);
             }
